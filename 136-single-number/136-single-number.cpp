7e326d2dc7f063e72881a2ac1ce6e6998998c0c5
chavananiket38 <88279602+chavananiket38@users.noreply.github.com>
2022-02-20 17:38:05 +0530

    Time: 35 ms (29.57%), Space: 17 MB (49.32%) - LeetHub

--- 136-single-number/136-single-number.cpp
+++ 136-single-number/136-single-number.cpp
@@ -1,10 +1,21 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        int ans = 0;
-        for(auto i:nums)
-            ans ^= i;
-        
-        return ans;
+        sort(nums.begin(), nums.end()); 
+        nums.push_back(INT_MAX);
+        int count;
+        if(nums.size() == 1){
+            count = *nums.begin();
+        }else{
+        for(int i = 0 ; i < nums.size(); i++){
+           if(nums[i] != nums[i+1]){
+                count = nums[i];
+               break;
+           } else{
+               i++;
+           }
+         }
+        }
+        return count;
     }
 };
