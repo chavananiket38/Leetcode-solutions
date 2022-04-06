f75368bfc8c13efdbf4a15187c7d9767956aa927
chavananiket38 <88279602+chavananiket38@users.noreply.github.com>
2022-04-06 10:25:18 +0530

    Time: 188 ms (30.12%), Space: 21.3 MB (39.95%) - LeetHub

--- 15-3sum/15-3sum.cpp
+++ 15-3sum/15-3sum.cpp
@@ -5,28 +5,30 @@ public:
         if(nums.size()<=2)return ans;
         sort(nums.begin(), nums.end());
         
-        for(int i=0; i<nums.size()-2; i++){
-           if(i==0 || (i>0 && nums[i] != nums[i-1])) {
+        int n = nums.size();
+        for(int i=0 ; i<n-2; i++){
+            if(i==0 || (nums[i]!=nums[i-1])){
                 int lo = i+1;
-                int hi = nums.size()-1;
-               while(lo<hi){
+                int hi = n-1;
+                
+                while(lo<hi){
                     
-                    if(nums[lo]+nums[hi]==(-nums[i])){
-                        vector<int> v;
-                        v.push_back(nums[i]);
-                        v.push_back(nums[lo]);
-                        v.push_back(nums[hi]);
+                    if((nums[lo]+nums[hi]) == (-nums[i])){
+                        vector<int> v = {nums[i], nums[lo], nums[hi]};
                         ans.push_back(v);
-
-                        while(lo<hi && nums[lo]==nums[lo+1])lo++;
+                        
+                        while(lo<hi && nums[lo]==nums[lo+1])
+                            lo++;
+                        
                         while(lo<hi && nums[hi]==nums[hi-1])hi--;
                         
                         lo++;
                         hi--;
                     }
-
-                    else if(nums[lo]+nums[hi]>(-nums[i]))hi--;
-                    else lo++;
+                    else if((nums[lo]+nums[hi]) > (-nums[i]))
+                        hi--;
+                    else 
+                        lo++;
                 }
             }
         }
