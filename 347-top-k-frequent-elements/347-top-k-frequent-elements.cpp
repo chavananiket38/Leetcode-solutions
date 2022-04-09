9da93f1014dfb52f552b853a18ebceb459874c01
chavananiket38 <88279602+chavananiket38@users.noreply.github.com>
2022-04-09 13:24:15 +0530

    Time: 21 ms (54.49%), Space: 13.8 MB (41.62%) - LeetHub

---
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.cpp
@@ -0,0 +1,26 @@
+bool cmp(const pair<int, int>& a, const pair<int, int>& b) {
+        return a.second > b.second;
+    }
+class Solution {
+    
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        map<int, int> mp;
+        for(auto i:nums){
+            mp[i]++;
+        }
+        
+        vector<pair<int, int>> v;
+        for(auto i:mp){
+            v.push_back( make_pair(i.first, i.second));
+        }
+        
+        sort(v.begin(), v.end(), cmp);
+        vector<int> ans;
+        
+        for(int i=0; i<k; i++)
+            ans.push_back(v[i].first);
+        
+        return ans;
+    }
+};
