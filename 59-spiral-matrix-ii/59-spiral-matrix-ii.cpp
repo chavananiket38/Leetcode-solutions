b4e80c9a693b5a4dd9eec31737ee8c1fa95909ac
chavananiket38 <88279602+chavananiket38@users.noreply.github.com>
2022-04-13 22:34:55 +0530

    Time: 4 ms (19.27%), Space: 6.7 MB (18.13%) - LeetHub

--- 59-spiral-matrix-ii/59-spiral-matrix-ii.cpp
+++ 59-spiral-matrix-ii/59-spiral-matrix-ii.cpp
@@ -1,48 +1,37 @@
 class Solution {
 public:
     vector<vector<int>> generateMatrix(int n) {
-        vector<vector<int>> spiral(n, vector<int> (n));
-        if(n==0) return spiral;
+        vector<vector<int>> spiral (n, vector<int> (n));
         
         int startRow = 0;
-        int startCol = 0;
         int endRow = n-1;
+        int startCol = 0;
         int endCol = n-1;
         
         int cnt = 0;
-        int size = n*n;
+        int end = n*n;
         
-        while(cnt<size){
+        while(cnt<end){
             
-            for(int i=startCol; cnt<size && i<=endCol; i++){
-                
-                cnt++;
-                spiral[startRow][i] = cnt;
-                
-            }
+            for(int i=startCol; cnt<end && i<=endCol; i++)
+                spiral[startRow][i] = ++cnt;
+            
             startRow++;
             
-            for(int i=startRow; cnt<size && i<=endRow; i++){
-                cnt++;
-                spiral[i][endCol] = cnt;
-               
-            }
+            for(int i=startRow; cnt<end && i<=endRow; i++)
+                spiral[i][endCol] = ++cnt;
+            
             endCol--;
             
-            for(int i=endCol; cnt<size && i>=startCol; i--){
-                cnt++;
-                spiral[endRow][i] = cnt;
-                
-            }
+            for(int i=endCol; cnt<end && i>=startCol; i--)
+                spiral[endRow][i] = ++cnt;
+            
             endRow--;
             
-            for(int i=endRow; cnt<size && i>=startRow; i--){
-                cnt++;
-                spiral[i][startCol] = cnt;
-                
-            }
-            startCol++;
+            for(int i=endRow; cnt<end && i>=startRow; i--)
+                spiral[i][startCol] = ++cnt;
             
+            startCol++;
         }
         
         return spiral;
