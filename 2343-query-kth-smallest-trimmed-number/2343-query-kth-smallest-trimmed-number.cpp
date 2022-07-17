cea54dc029290510c3711711f6d29782e6c97905
chavananiket38 <88279602+chavananiket38@users.noreply.github.com>
2022-07-17 10:20:28 +0530

    Time: 1508 ms (11.11%), Space: 372.4 MB (11.11%) - LeetHub

--- 2343-query-kth-smallest-trimmed-number/2343-query-kth-smallest-trimmed-number.cpp
+++ 2343-query-kth-smallest-trimmed-number/2343-query-kth-smallest-trimmed-number.cpp
@@ -1,42 +1,38 @@
 class Solution {
 public:
-//     vector<int> smallestTrimmedNumbers(vector<string>& nums, vector<vector<int>>& queries) {
-//         vector<int> ans;
-//         int size = nums[0].size();
-        
-//         for(int i=0; i<queries.size(); i++){
-//             vector<string> v = nums;
-            
-//             int maxIdx = queries[i][0];
-//             int trim = queries[i][1];
-//             for(int i=0; i<v.size(); i++){
-//                 v[i] = v[i].substr(size-trim);
-//             }
-            
-//             vector<pair<string,int>> mp;
-//             for(int i=0; i<v.size(); i++){
-//                 mp.push_back({v[i], i});
-//             }
-        
-//             sort(mp.begin(), mp.end());
-//             ans.push_back(mp[maxIdx-1].second);
-            
-//         }
-        
-//         return ans;
-//     }
-    
     vector<int> smallestTrimmedNumbers(vector<string>& nums, vector<vector<int>>& queries) {
-        vector<int> res;
-        int n = nums.size();
-        for(auto &q:queries) {
-            vector<pair<string,int>> pq;
-            for(int i=0;i<n; i++) {
-                pq.push_back({(nums[i].substr(nums[i].size()-q[1])), i});
+        vector<int> ans;
+        int size = nums[0].size();
+        
+        for(int i=0; i<queries.size(); i++){
+            
+            int maxIdx = queries[i][0];
+            int trim = queries[i][1];
+            
+            vector<pair<string,int>> mp;
+            for(int i=0; i<nums.size(); i++){
+                mp.push_back({nums[i].substr(size-trim), i});
             }
-            sort(pq.begin(), pq.end());
-            res.push_back(pq[q[0]-1].second);
+            
+            sort(mp.begin(), mp.end());
+            ans.push_back(mp[maxIdx-1].second);
+            
         }
-        return res;
+        
+        return ans;
     }
+    
+    // vector<int> smallestTrimmedNumbers(vector<string>& nums, vector<vector<int>>& queries) {
+    //     vector<int> res;
+    //     int n = nums.size();
+    //     for(auto &q:queries) {
+    //         vector<pair<string,int>> pq;
+    //         for(int i=0;i<n; i++) {
+    //             pq.push_back({(nums[i].substr(nums[i].size()-q[1])), i});
+    //         }
+    //         sort(pq.begin(), pq.end());
+    //         res.push_back(pq[q[0]-1].second);
+    //     }
+    //     return res;
+    // }
 };
