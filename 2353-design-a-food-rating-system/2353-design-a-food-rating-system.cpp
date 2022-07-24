78dcda0a2db919400f27b5163b2cdceeb7953cd5
chavananiket38 <88279602+chavananiket38@users.noreply.github.com>
2022-07-24 20:07:37 +0530

    Time: 1385 ms (33.33%), Space: 166.2 MB (33.33%) - LeetHub

--- 2353-design-a-food-rating-system/2353-design-a-food-rating-system.cpp
+++ 2353-design-a-food-rating-system/2353-design-a-food-rating-system.cpp
@@ -1,11 +1,11 @@
 class FoodRatings {
 public:
-    unordered_map<string, set<pair<int, string>>> cuisine_rating;
-    unordered_map<string, string> food_cuisine;
-    unordered_map<string, int> food_rating;
+    map<string, set<pair<int, string>>> cuisine_rating_food;
+    map<string, string> food_cuisine;
+    map<string, int> food_rating;
     FoodRatings(vector<string>& foods, vector<string>& cuisines, vector<int>& ratings) {
         for(int i=0; i<foods.size(); i++){
-            cuisine_rating[cuisines[i]].insert({-ratings[i], foods[i]});
+            cuisine_rating_food[cuisines[i]].insert({-ratings[i], foods[i]});
             food_cuisine[foods[i]] = cuisines[i];
             food_rating[foods[i]] = ratings[i];
         }
@@ -13,13 +13,13 @@ public:
     
     void changeRating(string food, int newRating) {
         auto &c = food_cuisine.find(food)->second;
-        cuisine_rating[c].erase({-food_rating[food], food});
-        cuisine_rating[c].insert({-newRating, food});
+        cuisine_rating_food[c].erase({-food_rating[food], food});
+        cuisine_rating_food[c].insert({-newRating, food});
         food_rating[food] = newRating;
     }
     
     string highestRated(string cuisine) {
-        return begin(cuisine_rating[cuisine])->second;
+        return begin(cuisine_rating_food[cuisine])->second;
     }
 };
 
