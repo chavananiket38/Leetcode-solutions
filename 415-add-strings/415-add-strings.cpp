2229ad51662a8f9bd03ec7ba9933097bd322057c
chavananiket38 <88279602+chavananiket38@users.noreply.github.com>
2022-07-26 19:22:14 +0530

    Time: 6 ms (65.57%), Space: 6.6 MB (89.92%) - LeetHub

--- 415-add-strings/415-add-strings.cpp
+++ 415-add-strings/415-add-strings.cpp
@@ -1,58 +1,23 @@
 class Solution {
 public:
     string addStrings(string num1, string num2) {
-        int m = num1.size();
-        int n = num2.size();
-        vector<int> arr(n+1,0);
+        int i = num1.size()-1;
+        int j = num2.size()-1;
         
-        if(m>n){
-            arr.resize(m+1,0);
+        int carry = 0;
+        string res = "";
+        
+        while(i>=0 || j>=0 || carry){
+            long temp = 0;
+            if(i>=0){temp += num1[i]-'0'; i--;}
+            if(j>=0){temp += num2[j]-'0';j--;}
+            
+            temp += carry;
+            carry = temp/10;
+            res += (temp%10)+'0';
         }
         
-        int i = m-1, j = n-1;
-        while(i>=0 and j>=0){
-            if(i>=j){
-                int temp = (arr[i+1] + (num1[i]-'0')+(num2[j]-'0'));
-                arr[i+1] = temp%10;
-                arr[i] = temp/10;
-            }else{
-                int temp = (arr[j+1] + (num1[i]-'0')+(num2[j]-'0'));
-                arr[j+1] = temp%10;
-                arr[j] = temp/10;
-            }
-            i--;
-            j--;
-        }
-        
-        if(j!=0 && i>=0){
-            while(i>=0){
-                int temp = arr[i+1] + (num1[i]-'0');
-                arr[i+1] = temp%10;
-                arr[i] = temp/10;
-                i--;
-            }
-        }
-        if(i!=0 && j>=0){
-            while(j>=0){
-                int temp = arr[j+1] + (num2[j]-'0');
-                arr[j+1] = temp%10;
-                arr[j] = temp/10;
-                j--;
-            }
-        }
-        
-        for(auto i:arr)
-            cout<<i<<" ";
-        string ans = "";
-        for(i=0; i<arr.size(); i++)
-            if(arr[i]!=0)
-                break;
-        
-        for(int k=i; k<arr.size(); k++)
-            ans += arr[k]+'0';
-        
-        if(ans=="")
-            return "0";
-        return ans;
+        reverse(res.begin(), res.end());
+        return res;
     }
 };
