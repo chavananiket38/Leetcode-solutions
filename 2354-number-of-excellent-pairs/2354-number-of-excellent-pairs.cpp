8041d2e281390d2f1487270ec4a33094f90bb063
chavananiket38 <88279602+chavananiket38@users.noreply.github.com>
2022-08-03 19:13:27 +0530

    Time: 521 ms (60.55%), Space: 86.7 MB (79.10%) - LeetHub

--- 2354-number-of-excellent-pairs/2354-number-of-excellent-pairs.cpp
+++ 2354-number-of-excellent-pairs/2354-number-of-excellent-pairs.cpp
@@ -1,56 +1,18 @@
 class Solution {
 public:
-    
-    int search(vector<int> &arr, int r, int x){
-        int l = 0;
-        int res = -1;
-        
-        while(l<=r){
-            int mid = (l+r)/2;
-            
-            if(arr[mid]>=x){
-                res = mid;
-                r = mid-1;
-            }else{
-                l = mid+1;
-            }
-        }
-        return res;
-    }
-    
     long long countExcellentPairs(vector<int>& nums, int k) {
-        set<int> st;
-        for(auto i:nums)
-            st.insert(i);
-        vector<int> v;
+        unordered_set<int> st(nums.begin(), nums.end());
+        vector<int> cnt(30,0);
+        
         for(auto i:st)
-            v.push_back(i);
+            ++cnt[__builtin_popcount(i)];
+        long long res = 0;
         
-        vector<int> arr;
+        for(int i=1; i<30; i++)
+            for(int j=1; j<30; j++)
+                if(i+j>=k)
+                    res += cnt[i]*cnt[j];
         
-        for(auto i:v){
-            int k = i;
-            int cnt = 0;
-            while(k){
-                k = k&(k-1);
-                cnt++;
-            }
-            arr.push_back(cnt);
-        }
-        
-        sort(arr.begin(), arr.end());
-        long long ans = 0;
-        for(int i=0; i<arr.size(); i++){
-            if(arr[i]>=k)
-                ans+= i*2+1;
-            else{
-                int idx = search(arr, i, k-arr[i]);
-                
-                if(idx!=-1)
-                    ans += (i-idx)*2+1;
-            }
-        }
-        
-        return ans;
+        return res;
     }
 };
