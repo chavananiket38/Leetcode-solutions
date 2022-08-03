436a80ce6467f9884dd75b115dd0e60aa5bab51c
chavananiket38 <88279602+chavananiket38@users.noreply.github.com>
2022-08-03 19:43:19 +0530

    Time: 367 ms (12.64%), Space: 37.5 MB (94.67%) - LeetHub

--- 729-my-calendar-i/729-my-calendar-i.cpp
+++ 729-my-calendar-i/729-my-calendar-i.cpp
@@ -1,15 +1,7 @@
 class MyCalendar {
 public:
     vector<pair<int,int>> v;
-    MyCalendar() {
-        
-    }
-    
     bool book(int start, int end) {
-        if(v.empty()){
-            v.push_back({start,end});
-            return true;
-        }
         
         for(int i=0; i<v.size(); i++){
             if(start>=v[i].first && start<v[i].second)
@@ -19,6 +11,7 @@ public:
             if(v[i].first>start and v[i].second<=end)
                 return false;
         }
+        
         v.push_back({start,end});
         return true;
     }
