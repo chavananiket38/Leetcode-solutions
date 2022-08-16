3e19188c152ade44d0d7a9c4a753912af24bddf2
chavananiket38 <88279602+chavananiket38@users.noreply.github.com>
2022-08-16 19:34:02 +0530

    Time: 16 ms (30.98%), Space: 13.1 MB (47.67%) - LeetHub

--- 994-rotting-oranges/994-rotting-oranges.cpp
+++ 994-rotting-oranges/994-rotting-oranges.cpp
@@ -3,49 +3,42 @@
 class Solution {
 public:
     int orangesRotting(vector<vector<int>>& grid) {
-        int n = grid.size();
-        int m = grid[0].size();
-        
-        queue<tuple<int,int,int>> q;
+        int m = grid.size();
+        int n = grid[0].size();
         
         int cntOrg = 0;
+        queue<tuple<int,int,int>> q;
         
-        for(int i=0; i<n; i++)
-            for(int j=0; j<m; j++){
-                if(grid[i][j]==2)q.push(make_tuple(i, j, 0));
-                
-                if(grid[i][j]!=0)cntOrg++;
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                if(grid[i][j]==2) q.push(make_tuple(i, j, 0));
+                if(grid[i][j]!=0) cntOrg++;
             }
+        }
         
-        int cnt = 0;
-        int ans = 0;
-        
+        int ans = 0, cnt = 0;
+        int dir[] = {1, 0, -1, 0, 1};
         while(!q.empty()){
             int x, y, time;
-            tuple<int,int,int> tp = q.front();
-            
-            tie(x, y, time) = tp;
-            
+            tie(x, y, time) = q.front();
             q.pop();
             
             cnt++;
             ans = max(ans, time);
             
-            int dx[] = {1, 0, -1, 0};
-            int dy[] = {0, -1, 0, 1};
-            
             for(int i=0; i<4; i++){
-                int newX = x + dx[i];
-                int newY = y + dy[i];
+                int newX = x + dir[i];
+                int newY = y + dir[i+1];
                 
-                if(newX>=0 and newX<n and newY>=0 and newY<m && grid[newX][newY]==1){
+                if(newX>=0 && newX<m && newY>=0 && newY<n && grid[newX][newY]==1){
                     grid[newX][newY] = 2;
                     q.push(make_tuple(newX, newY, time+1));
                 }
             }
+            
         }
         
-        if(cnt==cntOrg)return ans;
+        if(cntOrg==cnt) return ans;
         return -1;
     }
 };
