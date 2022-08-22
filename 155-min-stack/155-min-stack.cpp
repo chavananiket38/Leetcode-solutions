c2528e2b04bd68d7844191b13889502d47061aca
chavananiket38 <88279602+chavananiket38@users.noreply.github.com>
2022-08-22 19:22:01 +0530

    Time: 19 ms (97.30%), Space: 16.4 MB (31.38%) - LeetHub

--- 155-min-stack/155-min-stack.cpp
+++ 155-min-stack/155-min-stack.cpp
@@ -1,32 +1,26 @@
 class MinStack {
 public:
-    stack<int> st1;
-    map<int,int> mp;
+    vector<pair<int,int>> st;
     MinStack() {
         
     }
     
     void push(int val) {
-        st1.push(val);
-        mp[val]++;
+        if(st.empty())
+            st.emplace_back(val, val);
+        else st.emplace_back(val, min(st.back().second, val));
     }
     
     void pop() {
-        int k = st1.top();
-        st1.pop();
-        if(mp[k]==1)
-            mp.erase(k);
-        else mp[k]--;
+        st.pop_back();
     }
     
     int top() {
-        return st1.top();
+        return st.back().first;
     }
     
     int getMin() {
-        for(auto i:mp)
-            return i.first;
-        return 0;
+        return st.back().second;
     }
 };
 
