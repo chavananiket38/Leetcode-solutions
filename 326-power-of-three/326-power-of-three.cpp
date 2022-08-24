ab89d4d42fbe2ca7e87c8401fb29df46279caf62
chavananiket38 <88279602+chavananiket38@users.noreply.github.com>
2022-08-25 00:09:49 +0530

    Time: 39 ms (38.01%), Space: 5.9 MB (29.00%) - LeetHub

--- 326-power-of-three/326-power-of-three.cpp
+++ 326-power-of-three/326-power-of-three.cpp
@@ -1,19 +1,7 @@
 class Solution {
 public:
-    bool helper(long long i, int k){
-        if(i>k)
-            return false;
-        if(i==k)
-            return true;
-        
-        return helper(3*i, k);
-    }
-    
     bool isPowerOfThree(int n) {
-        if(n==1)
-            return true;
-        if(n<=0)
-            return false;
-        return helper(3, n);
+        int k = pow(3, 19);
+        return (n>0 && k % n==0);
     }
 };
