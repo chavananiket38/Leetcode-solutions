4b9e42ee4f3eec9d526064f86e605bfc0b398581
chavananiket38 <88279602+chavananiket38@users.noreply.github.com>
2022-08-28 21:59:16 +0530

    Time: 23 ms (88.73%), Space: 14.9 MB (20.57%) - LeetHub

--- 210-course-schedule-ii/210-course-schedule-ii.cpp
+++ 210-course-schedule-ii/210-course-schedule-ii.cpp
@@ -1,53 +1,37 @@
 class Solution {
 public:
-    bool isCycle(int node, vector<vector<int>> &edges, vector<int> &vis, 
-                 vector<int> &dfsVis){
+    
+    bool toposort(int node, vector<vector<int>> &edges, vector<int> &vis, 
+                  stack<int> &st, vector<int> &dfsVis){
         vis[node] = 1;
         dfsVis[node] = 1;
         
         for(auto i:edges[node]){
             if(vis[i]==0){
-                if(isCycle(i, edges, vis, dfsVis))
+                if(toposort(i, edges, vis, st, dfsVis))
                     return true;
             }else if(dfsVis[i])
                 return true;
         }
         
         dfsVis[node] = 0;
-        return false;
-    }
-    
-    void toposort(int node, vector<vector<int>> &edges, vector<int> &vis, 
-                  stack<int> &st){
-        vis[node] = 1;
-        
-        for(auto i:edges[node]){
-            if(vis[i]==0)
-                toposort(i, edges, vis, st);
-        }
-        
         st.push(node);
+        return false;
     }
     
     vector<int> findOrder(int numC, vector<vector<int>>& pre) {
         vector<vector<int>> edges(numC);
-        
         for(auto i:pre){
             edges[i[0]].push_back(i[1]);
         }
         
         vector<int> vis(numC, 0), dfsVis(numC, 0);
-        for(int i=0; i<numC; i++){
-            if(vis[i]==0)
-                if(isCycle(i, edges, vis, dfsVis))
-                    return {};
-        }
-        
         stack<int> st;
-        fill(vis.begin(), vis.end(), 0);
+        
         for(int i=0; i<numC; i++){
             if(vis[i]==0)
-                toposort(i, edges, vis, st);
+                if(toposort(i, edges, vis, st, dfsVis))
+                    return {};
         }
         
         vector<int> res;
