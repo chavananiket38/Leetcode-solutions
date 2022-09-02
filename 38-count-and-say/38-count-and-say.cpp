ab9ab0d771fad2acb95b7b8d402e50daf965edb8
chavananiket38 <88279602+chavananiket38@users.noreply.github.com>
2022-09-02 18:18:05 +0530

    Time: 17 ms (40.30%), Space: 6.5 MB (78.43%) - LeetHub

--- 38-count-and-say/38-count-and-say.cpp
+++ 38-count-and-say/38-count-and-say.cpp
@@ -1,19 +1,17 @@
 class Solution {
 public:
     string countAndSay(int n) {
-        if(n==1) return "1";
-        if(n==2) return "11";
-        
-        string lastStr = countAndSay(n-1);
-        int i=0, len = lastStr.size();
-        string ans = "";
-        
-        while(i<len){
-            int j = i;
-            while(i<len && lastStr[i]==lastStr[j]) i++;
-            ans += to_string(i-j) + lastStr[j];
+        string ans = "1";
+        for(int k=2; k<=n; k++){
+            string lastStr = ans;
+            int i=0, len = lastStr.size();
+            ans = "";
+            while(i<len){
+                int j = i;
+                while(i<len && lastStr[i]==lastStr[j]) i++;
+                ans += to_string(i-j) + lastStr[j];
+            }
         }
-        
         return ans;
     }
 };
